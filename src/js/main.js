bb9a42710c555221f6f9ec83e0ff7b99424b6ec4
Anatoly <povly19995@gmail.com>
2024-03-06 14:02:45 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const mains = document.querySelectorAll('.main');
-    if (mains[0]){
-        mains.forEach((main)=>{
+    if (mains[0]) {
+        mains.forEach((main) => {
             const _swiperElement = main.querySelector('.swiper');
             const _swiperPagination = main.querySelector(".swiper-pagination");
             const _swiper = new Swiper(_swiperElement, {
                 speed: _swiperElement.dataset.speed,
-                spaceBetween: 50,
+                spaceBetween: 10,
                 loop: true,
                 autoplay: {
                     delay: _swiperElement.dataset.autoplay,
@@ -17,12 +17,20 @@ document.addEventListener("DOMContentLoaded", () => {
                     el: _swiperPagination,
                     clickable: true,
                 },
+                breakpoints: {
+                    320: {
+                        allowTouchMove: true,
+                    },
+                    1200: {
+                        allowTouchMove: false,
+                    }
+                },
                 on: {
-                    afterInit: function (swiper){
+                    afterInit: function (swiper) {
                         _swiperPagination.style.setProperty('--count', swiper.pagination.bullets.length);
                         main.querySelector('.main__nums-end').textContent = swiper.pagination.bullets.length < 10 ? '0' + swiper.pagination.bullets.length : swiper.pagination.bullets.length;
                     },
-                    slideChange: function (swiper){
+                    slideChange: function (swiper) {
                         main.querySelector('.main__nums-start').textContent = swiper.realIndex + 1 < 10 ? '0' + (swiper.realIndex + 1) : swiper.realIndex + 1;
                     }
                 }
@@ -31,11 +39,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const interTels = document.querySelectorAll('.p-inter-tel');
-    if (interTels[0]){
-        interTels.forEach((interTel)=>{
+    if (interTels[0]) {
+        interTels.forEach((interTel) => {
             const slug = interTel.dataset.slug;
             const lang = interTel.dataset.lang;
-            const langUpper = lang.charAt(0).toUpperCase() +  lang.slice(1);
+            const langUpper = lang.charAt(0).toUpperCase() + lang.slice(1);
 
             const prefix = interTel.querySelector('.p-inter-tel__input-item span');
             const input = interTel.querySelector('.p-inter-tel__input-item input');
@@ -47,9 +55,9 @@ document.addEventListener("DOMContentLoaded", () => {
             const optionsUl = options.querySelector('ul');
             const optionsSearch = options.querySelector('input');
 
-            button.addEventListener('click', ()=>{
-                if (interTel.classList.contains('active')){
-                    _options.forEach((option)=>{
+            button.addEventListener('click', () => {
+                if (interTel.classList.contains('active')) {
+                    _options.forEach((option) => {
                         option.classList.remove('hidden');
                         optionsSearch.value = '';
                     })
@@ -65,10 +73,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 lazy: false,
             });
 
-            _countries.forEach((country)=>{
+            _countries.forEach((country) => {
                 const classes = slug === country.countryCode.toLowerCase() ? 'active' : '';
                 const optionOutput = `
-                    <li class="p-inter-tel__option flex flex-ai-center ${classes}" data-country="${country['name' + langUpper]}" data-code="${country.countryCode.toLowerCase()}" data-num="+${country.phoneCode}" data-mask="${country.phoneMask}">
+                    <li class="p-inter-tel__option ${classes}" data-country="${country['name' + langUpper]}" data-code="${country.countryCode.toLowerCase()}" data-num="+${country.phoneCode}" data-mask="${country.phoneMask}">
                       <div class="fi fi-${country.countryCode.toLowerCase()}"></div>
                       <div class="p-inter-tel__option-name">${country['name' + langUpper]}</div>
                       <div class="p-inter-tel__option-num">+${country.phoneCode}-${country.phoneMask}</div>
@@ -76,7 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 `;
                 optionsUl.insertAdjacentHTML('beforeend', optionOutput);
 
-                if (slug === country.countryCode.toLowerCase()){
+                if (slug === country.countryCode.toLowerCase()) {
                     prefix.textContent = `+${country.phoneCode}`;
                     buttonFlag.className = `fi fi-${country.countryCode.toLowerCase()}`;
                     input.placeholder = country.phoneMask;
@@ -89,14 +97,14 @@ document.addEventListener("DOMContentLoaded", () => {
             });
             _options = optionsUl.querySelectorAll('.p-inter-tel__option');
 
-            _options.forEach((option)=>{
-                option.addEventListener('click', ()=>{
+            _options.forEach((option) => {
+                option.addEventListener('click', () => {
                     const num = option.dataset.num;
                     const mask = option.dataset.mask;
                     const code = option.dataset.code;
 
                     const activeOption = optionsUl.querySelector('.p-inter-tel__option.active');
-                    if (activeOption){
+                    if (activeOption) {
                         activeOption.classList.remove('active');
                     }
 
@@ -116,9 +124,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 })
             })
 
-            optionsSearch.addEventListener('input', ()=>{
+            optionsSearch.addEventListener('input', () => {
                 let search_query = optionsSearch.value.toLowerCase();
-                _options.forEach((option)=>{
+                _options.forEach((option) => {
                     let is_matched = option.dataset.country.toLowerCase().includes(search_query);
                     is_matched ? option.classList.remove('hidden') : option.classList.add('hidden');
                 })
@@ -127,8 +135,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const cards = document.querySelectorAll('.cards');
-    if (cards[0]){
-        cards.forEach((card)=>{
+    if (cards[0]) {
+        cards.forEach((card) => {
             const _swiperElement = card.querySelector('.swiper');
             const _swiperPagination = card.querySelector(".swiper-pagination");
             const _swiperScrollbar = card.querySelector(".swiper-scrollbar");
@@ -147,7 +155,6 @@ document.addEventListener("DOMContentLoaded", () => {
                     }
                 },
                 scrollbar: {
-                    draggable: true,
                     el: _swiperScrollbar,
                     snapOnRelease: true,
                 },
@@ -156,6 +163,29 @@ document.addEventListener("DOMContentLoaded", () => {
                     prevEl: card.querySelector(".swiper-button-prev"),
                 },
             });
+        })
+    }
+
+    const pSelects = document.querySelectorAll('.p-select');
+    if (pSelects[0]){
+        pSelects.forEach((pSelect)=>{
+            const input = pSelect.querySelector('.p-select__input');
+            const btn = pSelect.querySelector('.p-select__btn');
+            const current = btn.querySelector('.p-select__current');
+            const options = pSelect.querySelectorAll('.p-select__options .p-select__option');
+
+            btn.addEventListener('click', ()=>{
+                pSelect.classList.toggle('active');
+            })
+
+            options.forEach((option)=>{
+                option.addEventListener('click', ()=>{
+                    input.value = option.textContent;
+                    current.textContent = option.textContent;
+                    pSelect.classList.remove('active');
+                    btn.classList.add('active');
+                })
+            })
         })
     }
 
